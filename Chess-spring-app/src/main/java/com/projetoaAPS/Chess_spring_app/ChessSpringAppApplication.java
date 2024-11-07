27b0a7a119bfd32a5b8884916d6b3cade09ab61e
carol <999746@profe.sed.sc.gov.br>
2024-11-07 19:57:07 -0300

    Initial commit

---
+++ Chess-spring-app/src/main/java/com/projetoaAPS/Chess_spring_app/ChessSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.projetoaAPS.Chess_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessSpringAppApplication.class, args);
+	}
+
+}
---
+++ Chess-spring-app/src/test/java/com/projetoaAPS/Chess_spring_app/ChessSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoaAPS.Chess_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
